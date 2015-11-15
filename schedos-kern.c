ed1bc539e44bdd318ea4b938dcf00ca8a2e415b6
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-11-15 01:34:12 -0800

    attempt at solving exercise 2

--- schedos-kern.c
+++ schedos-kern.c
@@ -127,7 +127,7 @@ start(void)
 	//   41 = p_priority algorithm (exercise 4.a)
 	//   42 = p_share algorithm (exercise 4.b)
 	//    7 = any algorithm that you may implement for exercise 7
-	scheduling_algorithm = 0;
+	scheduling_algorithm = 1;
 
 	// Switch to the first process.
 	run(&proc_array[1]);
@@ -219,7 +219,7 @@ schedule(void)
 {
 	pid_t pid = current->p_pid;
 
-	if (scheduling_algorithm == 0)
+	if (scheduling_algorithm == 0) {	
 		while (1) {
 			pid = (pid + 1) % NPROCS;
 
@@ -229,6 +229,15 @@ schedule(void)
 			if (proc_array[pid].p_state == P_RUNNABLE)
 				run(&proc_array[pid]);
 		}
+	} else if (scheduling_algorithm == 1) {
+		while (1) {
+			if (proc_array[pid].p_state == P_RUNNABLE) {
+				run(&proc_array[pid]);
+			} else {
+				pid = (pid + 1) % NPROCS;
+			}
+		}
+	}
 
 	// If we get here, we are running an unknown scheduling algorithm.
 	cursorpos = console_printf(cursorpos, 0x100, "\nUnknown scheduling algorithm %d\n", scheduling_algorithm);
